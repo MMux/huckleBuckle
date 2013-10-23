be22f31588fe01d9ecfec4aab24c563c95f7ab4c
MMux <michaeljmoss@gmail.com>
2013-10-22 18:37:07 -0700

    not much progress

--- js/application.js
+++ js/application.js
@@ -2,8 +2,36 @@ $(function() {
     console.log("loaded");
 
 
-    var computerChoice = Math.random();
+    var computerChoice = Math.floor(Math.random() + min(0) max(100) +1)();
+    var firstChoice = function() {
+        var buttonClick = $("#submit").click();
+        };
+        
+console.log(firstChoice());
+       /*
+ buttonClick.addEventListener("click");
+     userChoice = function() {
+         
+*/
+/*
+     } 
+
+/*
+        if (computerChoice < userChoice) {
+            console.log("warm")
+        } else {
+            console.log("cold");
+        }
+*/
+    }
 
 });
 
 console.log(computerChoice);
+
+if (userChoice === computerChoice) {
+    console.log("Nicely Done! That's the answer!");
+} else {
+    var 
+}
+*/
