d0e6f041e595a52f995f63db20593315ad613c0e
MMux <michaeljmoss@gmail.com>
2013-10-26 04:19:58 -0700

    added function for game reset

--- js/application.js
+++ js/application.js
@@ -1,37 +1,40 @@
+var computerChoice = Math.floor(Math.random() * 100  +1);
+
 $(function() {
     console.log("loaded");
-
-
-    var computerChoice = Math.floor(Math.random() + min(0) max(100) +1)();
-    var firstChoice = function() {
-        var buttonClick = $("#submit").click();
-        };
-        
-console.log(firstChoice());
-       /*
- buttonClick.addEventListener("click");
-     userChoice = function() {
-         
-*/
-/*
-     } 
-
-/*
-        if (computerChoice < userChoice) {
-            console.log("warm")
-        } else {
-            console.log("cold");
-        }
-*/
-    }
-
+    var userChoice = 0;
+    var previousChoice = 0;
+        console.log(computerChoice);
+    $("#submit").click(function(){
+        userChoice = +$("#guess").val();
+        console.log(userChoice);
+        compareChoice(userChoice);
+        previousChoice = userChoice;
+})
 });
 
-console.log(computerChoice);
 
-if (userChoice === computerChoice) {
-    console.log("Nicely Done! That's the answer!");
-} else {
-    var 
+       
+   
+        
+var compareChoice = function(choice) {
+    if (choice === computerChoice) {
+        console.log("Nicely Done! That's the answer!");
+        $( "#answer" ).append( document.createTextNode( "Nicely done! That's the answer" ) );
+    } else if(true) {
+         console.log("This is the else statement");
+         
+    } else {
+         console.log("This is the else statement");
 }
-*/
+};
+
+var newGame = function() {
+   $("#reset").click(function(){
+    console.log("test");
+    $("#anwser").empty();
+})
+};
+
+
+
