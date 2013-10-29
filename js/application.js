059c0c24faf010e6aa5ca24d0aa2b3cd4fa9e7dd
MMux <michaeljmoss@gmail.com>
2013-10-29 15:25:48 -0700

    js file update

--- js/application.js
+++ js/application.js
@@ -2,8 +2,10 @@ var computerChoice = Math.floor(Math.random() * 100  +1);
 
 $(function() {
     console.log("loaded");
+    
     var userChoice = 0;
     var previousChoice = 0;
+    var answerArea = ("#answer");
         console.log(computerChoice);
     $("#submit").click(function(){
         userChoice = +$("#guess").val();
@@ -13,28 +15,41 @@ $(function() {
 })
 });
 
-
-       
-   
         
 var compareChoice = function(choice) {
     if (choice === computerChoice) {
         console.log("Nicely Done! That's the answer!");
         $( "#answer" ).append( document.createTextNode( "Nicely done! That's the answer" ) );
-    } else if(true) {
-         console.log("This is the else statement");
+    } else if(choice == ' ') {
+         console.log("Please enter a number");
+         $("#answer").append(document.createTextNode("Please enter a number"));
+         } else if((choice > 100) || (choice < 0)) {
+         console.log("Please enter a number between 0 and 100");
+         $("#answer").append(document.createTextNode("Please enter a number between 0 and 100"));
          
+    } else if () {
+    
+
+
     } else {
          console.log("This is the else statement");
 }
 };
 
-var newGame = function() {
-   $("#reset").click(function(){
-    console.log("test");
-    $("#anwser").empty();
-})
-};
+/*
 
+var newNumber = function() {
+    var computerChoice = newNumber();
+}
+*/
+
+var divClone = ("$answer");
+
+   $("#reset").click(function(){
+   $("#answer").replaceWith("Time for a new game! Make another guess.");
+    console.log("New Game");
+   /*  $("#answer"). */
+   $("#answer").replaceWith(divClone); 
+})
 
 
